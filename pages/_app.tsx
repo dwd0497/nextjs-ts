76e991aa880d38ef992dae964a92f02551ed00a9
pdenisov <dwd0497@gmail.com>
2022-02-03 20:43:54 +0500

    * начальная структура

---
+++ pages/_app.tsx
@@ -0,0 +1,18 @@
+import '../styles/globals.css';
+import {AppProps} from "next/dist/shared/lib/router/router";
+import Head from "next/head";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Top app</title>
+        <meta name="description" content="Learn project" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
