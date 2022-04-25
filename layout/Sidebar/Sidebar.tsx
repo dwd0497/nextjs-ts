e0dca3eb6e4bec7757627c681ea24b6286b2c792
pdenisov <dwd0497@gmail.com>
2022-04-25 17:37:23 +0500

    * Добавлен логотип и стили

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -1,11 +1,16 @@
 import React, { DetailedHTMLProps, HTMLAttributes } from 'react';
 import { Menu } from '../Menu/Menu';
+import Logo from '/public/images/logo.svg'
+import cn from "classnames";
+import styles from "./sidebar.module.scss";
 
 interface ISidebar extends DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement>{ }
 
-export const Sidebar = (props: ISidebar) => {
+export const Sidebar = ({className, ...props}: ISidebar) => {
   return (
-    <aside {...props}>
+    <aside className={cn(className, styles.sidebar)} {...props}>
+      <Logo />
+      <div>Поиск</div>
       <Menu />
     </aside>
   );
