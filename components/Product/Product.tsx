20dc1b91831687b9f2a6d3c10254d42673327e42
pdenisov <dwd0497@gmail.com>
2022-04-29 12:26:54 +0500

    * добавлен компонент Отзыва

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -9,6 +9,7 @@ import { Paragraph } from "../Paragraph/Paragraph";
 import { Button } from "../Button/Button";
 import { declDepOnNumber, priceAdapter } from "../../helpers/helpers";
 import Image from 'next/image'
+import { Review } from "../Review/Review";
 
 interface IProductComponent extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
   product: IProduct,
@@ -16,63 +17,68 @@ interface IProductComponent extends DetailedHTMLProps<HTMLAttributes<HTMLDivElem
 
 export const Product = ({ product, className, ...restProps }: IProductComponent) => {
   return (
-    <div className={cn(styles.product, className)} {...restProps}>
-      <div className={styles.product__header}>
-        <Image
-          src={process.env.NEXT_PUBLIC_DOMAIN + product.image}
-          alt={product.title}
-          width={70}
-          height={70}
-          className={styles.product__logo}
-        />
-        <Heading tag="h3" className={styles.product__title}>{product.title}</Heading>
-        <div className={styles.product__price}>
-          {priceAdapter(product.price)}
-          <Tag tagColor="green" className={styles.product__oldPrice}>- {priceAdapter(product.oldPrice - product.price)}</Tag>
+    <>
+      <div className={cn(styles.product, className)} {...restProps}>
+        <div className={styles.product__header}>
+          <Image
+            src={process.env.NEXT_PUBLIC_DOMAIN + product.image}
+            alt={product.title}
+            width={70}
+            height={70}
+            className={styles.product__logo}
+          />
+          <Heading tag="h3" className={styles.product__title}>{product.title}</Heading>
+          <div className={styles.product__price}>
+            {priceAdapter(product.price)}
+            <Tag tagColor="green" className={styles.product__oldPrice}>- {priceAdapter(product.oldPrice - product.price)}</Tag>
+          </div>
+          <p className={styles.product__credit}>{priceAdapter(product.credit)}<span>/мес</span></p>
+          <Rating rating={product.reviewAvg} className={styles.product__rating} />
+          {!!product.tags?.length && (
+            <ul className={styles.product__tags}>
+              {product.tags.map((tag) => <li key={tag} className={styles.product__tag}><Tag tagColor="ghost">{tag}</Tag></li>)}
+            </ul>
+          )}
+          <p className={styles.product__priceTitle}>цена</p>
+          <p className={styles.product__creditTitle}>в кредит</p>
+          <p className={styles.product__reviewCount}>
+            {product.reviewCount} {declDepOnNumber(product.reviewCount, ['отзыв', 'отзыва', 'отзывов'])}
+          </p>
         </div>
-        <p className={styles.product__credit}>{priceAdapter(product.credit)}<span>/мес</span></p>
-        <Rating rating={product.reviewAvg} className={styles.product__rating} />
-        {!!product.tags?.length && (
-          <ul className={styles.product__tags}>
-            {product.tags.map((tag) => <li key={tag} className={styles.product__tag}><Tag tagColor="ghost">{tag}</Tag></li>)}
-          </ul>
-        )}
-        <p className={styles.product__priceTitle}>цена</p>
-        <p className={styles.product__creditTitle}>в кредит</p>
-        <p className={styles.product__reviewCount}>
-          {product.reviewCount} {declDepOnNumber(product.reviewCount, ['отзыв', 'отзыва', 'отзывов'])}
-        </p>
+        <Paragraph className={styles.product__description}>{product.description}</Paragraph>
+        <div className={styles.product__body}>
+          {!!product.characteristics?.length && (
+            <ul className={styles.product__characteristics}>
+              {product.characteristics.map((characteristic) => (
+                <li className={styles.product__characteristic} key={characteristic.name}>
+                  <span className={styles.product__characteristicName}>{characteristic.name}</span>
+                  <span className={styles.product__characteristicDots} />
+                  <span className={styles.product__characteristicValue}>{characteristic.value}</span>
+                </li>
+              ))}
+            </ul>
+          )}
+          {product.advantages && (
+            <div className={styles.product__advantages}>
+              <span>Преимущества</span>
+              {product.advantages}
+            </div>
+          )}
+          {product.disadvantages && (
+            <div className={styles.product__disadvantages}>
+              <span>Недостатки</span>
+              {product.disadvantages}
+            </div>
+          )}
+        </div>
+        <div className={styles.product__buttons}>
+          <Button>Узнать подробнее</Button>
+          <Button appearance="second" arrow="right">Узнать подробнее</Button>
+        </div>
       </div>
-      <Paragraph className={styles.product__description}>{product.description}</Paragraph>
-      <div className={styles.product__body}>
-        {!!product.characteristics?.length && (
-          <ul className={styles.product__characteristics}>
-            {product.characteristics.map((characteristic) => (
-              <li className={styles.product__characteristic} key={characteristic.name}>
-                <span className={styles.product__characteristicName}>{characteristic.name}</span>
-                <span className={styles.product__characteristicDots} />
-                <span className={styles.product__characteristicValue}>{characteristic.value}</span>
-              </li>
-            ))}
-          </ul>
-        )}
-        {product.advantages && (
-          <div className={styles.product__advantages}>
-          <span>Преимущества</span>
-            {product.advantages}
-          </div>
-        )}
-        {product.disadvantages && (
-          <div className={styles.product__disadvantages}>
-            <span>Недостатки</span>
-            {product.disadvantages}
-          </div>
-        )}
-      </div>
-      <div className={styles.product__buttons}>
-        <Button>Узнать подробнее</Button>
-        <Button appearance="second" arrow="right">Узнать подробнее</Button>
-      </div>
-    </div>
+      {!!product.reviews?.length && (
+        product.reviews.map((review) => <Review key={review._id} review={review} />)
+      )}
+    </>
   );
 };
---
+++ components/Review/Review.tsx
@@ -0,0 +1,34 @@
+import React, { DetailedHTMLProps, HTMLAttributes } from 'react';
+import styles from './Review.module.scss';
+import { IProductReview } from "../../interfaces/product.interface";
+import cn from "classnames";
+import UserIcon from './user.svg'
+import { Rating } from "../Rating/Rating";
+import { Paragraph } from "../Paragraph/Paragraph";
+import { format } from "date-fns"
+import { ru } from "date-fns/locale"
+
+
+interface IReview extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+  review: IProductReview,
+}
+
+export const Review = ({ review, className, ...restProps }: IReview) => {
+  const { name, title , description, rating, createdAt} = review;
+
+  return (
+    <div className={cn(styles.review, className)} {...restProps}>
+      <div className={styles.review__header}>
+        <UserIcon className={styles.review__icon} />
+        <p className={styles.review__name}>{name}</p>
+        <p className={styles.review__title}>{title}</p>
+        <p className={styles.review__date}>{format(new Date(createdAt), "dd MMMM yyyy", {locale: ru})}</p>
+        <Rating rating={rating} />
+      </div>
+      <Paragraph size="s" className={styles.review__description}>{description}</Paragraph>
+      <div className={styles.review__form}>
+        Form
+      </div>
+    </div>
+  );
+};
